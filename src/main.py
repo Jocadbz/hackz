afb99ea7a7c73fe6bc6f39832a18a47d2b2d4adf
Jocadbz <madureirajoaquim95@gmail.com>
2022-06-13 21:27:42 -0300

    Fix code style according to PEP8
    Fun fact: the code is 0% readable now.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 import sys
 import time
 
+
 def weird_shit():
-     t_end = time.time() + 3
-     while time.time() < t_end:
-         print('\033[0;32m0101010110101010101010110111011111001010101100010100101\033[00m')
-         print('\033[0;32m0101010101010101010001101010101010110010101010101010101\033[00m')
-         print('\033[0;32ms0df78df6865sdd5df58sdcd5f86df585dv5d86dv5sd68v5sddd685\033[00m')
+    t_end = time.time() + 3
+    while time.time() < t_end:
+        print(green.start, '0101010110101010101010110111011111001010101100010',
+              green.end)
+        print(green.start, '0101010101010101010001101010101010110010101010101',
+              green.end)
+        print(green.start, '0df78df6865sdd5df58sdcd5f86df585dv5d86dv5sd68v5sd',
+              green.end)
 
 
 def among_us():
@@ -29,9 +33,11 @@ def among_us():
     while time.time() < t_end:
         print(green.start, sus, green.end)
 
+
 class green:
     start = "\033[0;32m"
     end = "\033[00m"
+
 
 maskguy = """
 ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
@@ -61,26 +67,31 @@ maskguy = """
          """
 
 
-
 def main():
     weird_shit()
     print(f"\033[0;32m{maskguy}\033[00m")
     print("")
-    print("This is a anonymus aproved hackz tools, so you can hakz everything hahahahahah")
+    print("This is a anonymus aproved hackz tools, so you can hakz everything")
     time.sleep(1)
-    print("are you sure you want to use vpn forever end get hacked an run away from police????!?!?!?!")
+    print("are u sure you want to use vpn forever end get hacked an run away",
+          "from police????!?!?!?!")
     hak = input("Yes or No? : ")
     if hak.lower() == 'n' or hak.lower() == 'N' or hak.lower() == 'no':
-        print("hahahahaha to late, wu will be hacked by china hackers!1!!!111!11!")
-        print("\033[0;32mSuper Idol的笑容 都没你的甜 八月正午的阳光 都没你耀眼 热爱 105 °C的你 滴滴清纯的蒸馏水\033[00m")
+        print("hahahahaha to late, wu will be hacked by china hackers!1!!!111")
+        print(green.start, "Super Idol的笑容 都没你的甜 八月正午的阳光 都没你耀眼 热爱 105 °C的你 滴滴",
+              green.end)
         time.sleep(3)
         t_end = time.time() + 6
         while time.time() < t_end:
-            print('\033[0;32m0101010110101010101010110111011111001010101100010100101\033[00m')
-            print('\033[0;32m0101010101010101010001101010101010110010101010101010101\033[00m')
-            print('\033[0;32ms0df78df6865sdd5df58sdcd5f86df585dv5d86dv5sd68v5sddd685\033[00m')
-            print("\033[0;32mSuper Idol的笑容 都没你的甜 八月正午的阳光 都没你耀眼 热爱 105 °C的你 滴滴清纯的蒸馏水\033[00m")
-        print("ALL OF YOUR FILES ARE GONE !!!11!!1!!11! AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+            print(green.start, '010101011010101010101011011101111100101010110',
+                  green.end)
+            print(green.start, '010101010101010101000110101010101011001010101',
+                  green.end)
+            print(green.start, '0df78df6865sdd5df58sdcd5f86df585dv5d86dv5sd68',
+                  green.end)
+            print(green.start, "Super Idol的笑容 都没你的甜 八月正午的阳光 都没你耀眼 热爱 105 °C的你",
+                  green.end)
+        print("ALL OF YOUR FILES ARE GONE !!!11!!1!!11! AAAAAAAAAAAAAAAAAAAAA")
         sys.exit(0)
     else:
         print(green.start, "yu are nou a member ofy anonimuos!", green.end)
@@ -90,12 +101,12 @@ def main():
     print(green.start, "TIME TO HACK NASA", green.end)
     time.sleep(2)
     weird_shit()
-    print(green.start, "YOU HACKED NASA AND YOU ARE THE MASTER HACK NOW!!!!!!!!!!!!!!!!!", green.end)
+    print(green.start, "YOU HACKED NASA AND YOU ARE THE MASTER HACK NOW!!!!!!",
+          green.end)
     time.sleep(2)
     among_us()
-    
-        
-        
+
+
 try:
     main()
 except KeyboardInterrupt:
